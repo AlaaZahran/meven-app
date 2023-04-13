4ed01076bfe503bad385ccb4c061e26eb341fbc5
AlaaZahran <46306526+AlaaZahran@users.noreply.github.com>
2023-04-13 02:11:39 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,4 @@
-def buildJar(){
-   echo 'building jar file ...'
-           sh 'mvn package'
+def deployAPP(){
+    echo 'deploying application ...'
 }
-def buildPushImage(){
-    echo ' building docker image .....'
-     withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-     sh 'docker build . -t alaa0ali/mavnapp:1.2'
-     echo "username is $USERNAME "
-     sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin" 
-     echo "push docker image to dockerhub"
-     sh 'docker push alaa0ali/mavnapp:1.2'
-     }}
 return this
